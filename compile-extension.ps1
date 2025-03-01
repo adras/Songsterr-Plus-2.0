33d401dfed2d8841769a7e5a9553a4a1e77c9d81
Andreas.Kurka <andreas.kurka@gmail.com>
2025-03-01 16:44:27 +0100

    Updated powershell scripts

--- compile-extension.ps1
+++ compile-extension.ps1
@@ -1,3 +1,21 @@
-Set-Location src;
-zip -r ..\web-ext-artifacts\ext.xpi *
+Set-Location src
+
+
+$zipPath = "..\extension.zip"
+$xpiPath = "..\SongsterrPlus.xpi"
+
+if (Test-Path $zipPath) {
+    Remove-Item -Path $zipPath -Force
+}
+
+if (Test-Path $xpiPath) {
+    Remove-Item -Path $xpiPath -Force
+}
+
+
+Compress-Archive -Path "*" -DestinationPath $zipPath
+
+
+Rename-Item -Path $zipPath -NewName "SongsterrPlus.xpi"
+
 Set-Location ..
--- fetch-scripts.ps1
+++ fetch-scripts.ps1
@@ -1,5 +1,5 @@
-foreach($b in $(Get-ChildItem -Path src/latest *Panel*)) {
+foreach($b in $(Get-ChildItem -Path src/latest **)) {
     Write-Output $b;
     $f=$b.Name
-    curl -L "https://www.songsterr.com/static/latest/$f" > src/latest/$f
+    & curl.exe -L "https://static2.songsterr.com/production-main/static2/latest/$f" -o "src/latest/$f"
 }
